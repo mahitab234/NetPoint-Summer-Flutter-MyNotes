2b1ecd6a08bf28bc3d4aaf1c737dc268ddc83f8f
mahitab234 <mahitab_20230441@fci.helwan.edu.eg>
2026-09-07 16:06:36 +0300

    Implement initial authentication screens and update entry point
    - Create boilerplate `SignInScreen` and `SignUpScreen` widgets in `lib/features/auth/screens`.
    - Update `main.dart` to set `SignInScreen` as the initial application route, replacing `HomeScreen`.
    - Refactor imports in `main.dart` to support the change in the initial screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'core/repositories/note_repository.dart';
-import 'features/home/screens/home_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'features/auth/screens/sign_in_screen.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -26,7 +26,7 @@ class MyNotes extends StatelessWidget {
       splitScreenMode: true,
       builder: (context, child) {
         return const MaterialApp(
-          home: HomeScreen(),
+          home: SignInScreen(),
           title: 'My Notes',
           debugShowCheckedModeBanner: false,
         );
